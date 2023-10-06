70bfbd6364d1d241525096dec9f0d5a9a21a4687
MD SAMIT INTESAR <saadsamit64@gmail.com>
2023-10-06 22:08:52 +0600

    setUp project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+function App() {
+
+  return (
+    <div className='Mydiv'>
+      <h1
+  className="font-extrabold textGradiend text-8xl"
+>
+  Tailwind CSS
+</h1>
+      <h1 className="MyBtn text-paraColor">Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
